c2e6704b19efea01aec170f41a11afc163b2c020
fiona-meng <fionameng2001@gmail.com>
2024-12-27 10:24:15 -0500

    basic framework

---
+++ script.js
@@ -0,0 +1,8 @@
+function showTranslation() {
+  var x = document.getElementById("translationCard");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
