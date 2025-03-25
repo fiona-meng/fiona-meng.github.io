1f405b31e9802b9b7d288f224b44e7c0c9226e0c
fiona-meng <fionameng2001@gmail.com>
2025-03-24 22:16:07 -0400

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-function showTranslation() {
-  var x = document.getElementById("translationCard");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
